4d5e6db3e9c64153b7921f18f95366c1cf1ec252
Eduardo Salazar <eduardosalazar89@hotmail.es>
2022-07-13 21:57:56 -0500

    Add delete note ui

--- resources/js/Pages/Notes/List.vue
+++ resources/js/Pages/Notes/List.vue
@@ -54,7 +54,7 @@
                                                     </svg>
                                                 </a>
 
-                                                <button @click="confirmDeleteion(note)" class="bg-transparent hover focus:outline-none">
+                                                <button @click="confirmDeletion(note)" class="bg-transparent hover focus:outline-none">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                                     </svg>
@@ -79,6 +79,33 @@
                                 </tr>
                             </tbody>
                         </table>
+                        <div v-if="isDeleting" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" id="create-note">
+                            <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                                <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">​</span>
+                                <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                                    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                                        <div class="sm:flex sm:items-start">
+                                            <h3 class="font-bold text-lg leading-6 text-gray-900" id="modal-title">
+                                                Delete note!!
+                                            </h3>
+                                        </div>
+                                        <div class="mt-2">
+                                            This action cannot be reversed, do you want to continue?
+                                        </div>
+                                    </div>
+                                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                        <button @click="deleteNote" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+                                            Save
+                                        </button>
+                                        <button @click="cancel = false" type="button" class="closeModal mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                                            Cancel
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -104,7 +131,14 @@ export default {
             q: '',
         },
         notes: [],
+        isConfirmDialogOpen: false,
+        note: null,
     }),
+    computed:{
+        isDeleting() {
+            return this.isConfirmDialogOpen && this.note
+        }
+    },
     methods: {
         moment: moment,
         numeral: numeral,
@@ -115,8 +149,21 @@ export default {
         search() {
             this.fetchNotes(this.params);
         },
-        editNote(note) {
-
+        confirmDeletion(note) {
+            this.note = note;
+            this.isConfirmDialogOpen = true;
+        },
+        cancel() {
+            this.note = null;
+            this.isConfirmDialogOpen = false;
+        },
+        async deleteNote() {
+            const {id} = this.note;
+            await axios.delete(`/api/notes/${id}`).then(({data}) => data);
+            const index = this.notes.indexOf(this.note);
+            this.notes.splice(index, 1);
+            this.note = null;
+            this.isConfirmDialogOpen = false;
         }
     },
     mounted() {
